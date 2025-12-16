517c11632505206dd412bec56358ba1aac2dedf3
semikk <kuz.sema@gmail.com>
2025-12-16 18:16:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+struct Zoo {
+  int Zebra;
+  int Leo;
+};
+
 int main() {
   std::cout << "Hello, world!" << std::endl;
+  Zoo x{5, 9};
+  std::cout << x.Zebra << std::endl;
   return 0;
 }
